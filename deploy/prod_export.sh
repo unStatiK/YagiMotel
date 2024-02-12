f3e30b89fdaa1da619d3613bd05817c7a430e96b
Steven Vch <unstatik@staremax.com>
2024-02-12 16:48:20 +0300

    update prod options

--- deploy/prod_export.sh
+++ deploy/prod_export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export YAGI_MOTEL_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ConcGCThreads=2 -Xms256M -Xmx512M"
+export YAGI_MOTEL_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ConcGCThreads=2 -Xms512M -Xmx512M"
